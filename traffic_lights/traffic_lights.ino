4b68472c84c319cb171a98d572e3b566ce022ec4
Hrishikesh Acharyya <acharyya.hrishi@gmail.com>
2025-07-12 17:05:25 +0530

    added pedestrian button
    Added a push button that pedestrians can press at any time to restart the cycle from red light.

--- traffic_lights/traffic_lights.ino
+++ traffic_lights/traffic_lights.ino
@@ -5,12 +5,14 @@ const int yellowLED = 11;
 const int yellowDelay = 4000;
 const int redLED = 12;
 const int redDelay = 5000;
+const int btnPin = 13;
 
 void setup()
 {
   pinMode(redLED, OUTPUT); //Red LED
   pinMode(yellowLED,OUTPUT);// yellow LED
   pinMode(greenLED,OUTPUT);// GREEN LED
+  pinMode(btnPin,INPUT_PULLUP); // when pushed, low signal passed. This way no external resistor needed
 }
 
 void loop()
@@ -18,18 +20,51 @@ void loop()
 
   // red light 5 seconds
   digitalWrite(redLED, HIGH);
-  delay(redDelay); // Wait for 1000 millisecond(s)
+  if(handleDelayandButtonPress(5000)) return ;
   digitalWrite(redLED, LOW);
+
 
 
   // green light 2 seconds
   digitalWrite(greenLED,HIGH);
-  delay(greenDelay);
+  if(handleDelayandButtonPress(2000)) return ;
   digitalWrite(greenLED,LOW);
   
   // yellow light 4 seconds 
   digitalWrite(yellowLED,HIGH);
-  delay(yellowDelay);
+  if(handleDelayandButtonPress(4000)) return ;
   digitalWrite(yellowLED,LOW);
  
+}
+
+bool handleDelayandButtonPress(long int delay)
+{
+  /*
+    Works like a delay but with condition
+    millis() - startchecks for time elapsed since led turned on
+
+    if inturrupted in between it will automatically restart cycle
+
+    else it will only run till delay given
+  */
+  long int start = millis();
+
+  while(millis() - start <= delay)
+  {
+    if(digitalRead(btnPin) == LOW)
+    {
+      handlePedestrian();
+      return true;
+    }
+  }
+
+  return false;
+
+}
+
+void handlePedestrian()
+{
+  digitalWrite(redLED, HIGH);
+  digitalWrite(yellowLED, LOW);
+  digitalWrite(greenLED, LOW);
 }
